e81d88a64d4d49149e472af59f312b472195225b
The19ThHell <the19thhell@the19thhell-Inspiron-1120.(none)>
2012-12-06 08:51:36 +0800

    sorted

--- ls2.c
+++ ls2.c
@@ -1,11 +1,18 @@
 #include	<stdio.h>
+#include	<stdlib.h>
+#include	<string.h>
 #include	<sys/types.h>
 #include	<dirent.h>
 /**
- **	ls version 1.0
+ **	ls version 2.0
  **		purpose  list contents of directory or directories
  **		action   if no args, use .  else list files in args
  **/
+
+int dir_cmp(const void *a,const void *b)
+{
+    return strcmp(((struct dirent *)a)->d_name,((struct dirent *)b)->d_name);
+}
 
 void do_ls(char *dirname)
 /*
@@ -13,14 +20,35 @@ void do_ls(char *dirname)
  */
 {
 	DIR *dir_ptr;		/* the directory */
-	struct dirent *direntp;		/* each entry	 */
+	struct dirent *direntp,**entrylist,**tmplist;		/* each entry	 */
+    int index,size;
 
 	if ((dir_ptr = opendir(dirname)) == NULL)
 		fprintf(stderr,"ls2: cannot open %s\n",dirname);
 	else
 	{
+        size = 100;
+        index = 0;
+        entrylist = (struct dirent **)malloc(sizeof(struct dirent*) * size);
 		while ((direntp = readdir(dir_ptr)) != NULL)
-			printf("%s\n",direntp->d_name);
+        {
+            if (direntp->d_name[0] == '.')
+                continue;
+            else
+            {
+                if (index >= size)
+                {
+                    tmplist = (struct dirent **)malloc(sizeof(struct dirent*) * size * 2);
+                    for (int i = 0;i < index;i++)
+                        tmplist[i] = entrylist[i];
+                    entrylist = tmplist;
+                }
+                entrylist[index++] = direntp;
+            }
+        }
+        qsort(entrylist,index - 1,sizeof(struct dirent*),dir_cmp);
+        for (int i = 0;i < index;i++)
+            printf("%s\n",entrylist[i]->d_name);
 		closedir(dir_ptr);
 	}
 }
