e71265e0f571152d91c69143519968666780d5e7
The19ThHell <the19thhell@the19thhell-Inspiron-1120.(none)>
2012-12-06 09:37:41 +0800

    sort fixed

--- ls2.c
+++ ls2.c
@@ -11,7 +11,7 @@
 
 int dir_cmp(const void *a,const void *b)
 {
-    return strcmp(((struct dirent *)a)->d_name,((struct dirent *)b)->d_name);
+    return strcmp((*(struct dirent **)a)->d_name,(*(struct dirent **)b)->d_name);
 }
 
 void do_ls(char *dirname)
@@ -29,7 +29,7 @@ void do_ls(char *dirname)
 	{
         size = 100;
         index = 0;
-        entrylist = (struct dirent **)malloc(sizeof(struct dirent*) * size);
+        entrylist = (struct dirent **)malloc(sizeof(struct dirent *) * size);
 		while ((direntp = readdir(dir_ptr)) != NULL)
         {
             if (direntp->d_name[0] == '.')
@@ -38,7 +38,7 @@ void do_ls(char *dirname)
             {
                 if (index >= size)
                 {
-                    tmplist = (struct dirent **)malloc(sizeof(struct dirent*) * size * 2);
+                    tmplist = (struct dirent **)malloc(sizeof(struct dirent *) * size * 2);
                     for (int i = 0;i < index;i++)
                         tmplist[i] = entrylist[i];
                     entrylist = tmplist;
@@ -46,7 +46,7 @@ void do_ls(char *dirname)
                 entrylist[index++] = direntp;
             }
         }
-        qsort(entrylist,index - 1,sizeof(struct dirent*),dir_cmp);
+        qsort(entrylist,index,sizeof(entrylist[0]),dir_cmp);
         for (int i = 0;i < index;i++)
             printf("%s\n",entrylist[i]->d_name);
 		closedir(dir_ptr);
@@ -58,7 +58,7 @@ int main(int argc, char *argv[])
 	if (argc == 1)
 		do_ls(".");
 	else
-		while (argc--){
+		while (--argc){
 			printf("%s:\n",*++argv);
 			do_ls(*argv);
 		}
